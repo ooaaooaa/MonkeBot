eb6285212d77bf59123aacd1daaf7ef84c0fdf64
ThatPumpkin <pcraftingpg@gmail.com>
2021-06-10 18:01:57 -0700

    Commit

---
+++ Modules/NotSafeForWork.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class NSFW(commands.Cog):
+    """booba & women (& men)"""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["boobs", "tits", "cleavage", "tiddies", "chunkers", "knockers", "chest"])
+    @commands.is_nsfw()
+    async def boobs(self, ctx):
+        """Fat on meat surrounded by skin"""
+        ctx.send("booba")
+
+def setup(bot):
+    bot.add_cog(NSFW(bot))
---
+++ Monke.py
@@ -0,0 +1,47 @@
+import discord
+import asyncio
+import os
+import json # For custom prefixes later on
+import traceback
+
+from dotenv import load_dotenv
+from discord.ext import commands
+from os.path import isfile, join
+
+# Loadeth dotenv
+load_dotenv()
+
+# Geteth tokeneth
+TOKEN = os.getenv('TOKEN')
+
+bot = commands.Bot(command_prefix = "m!",
+                   description = 'Monke Bot was made to fuel discord induced autism',
+                   case_insensitive=True) # Discord Bot <- BOT!
+                                          # (Some retarded tutorials will tell u to
+                                          # switch client and bot like FUCKING RETARDS)
+client = discord.Client(intents=intents) # Discord Client
+
+# Help is bad!
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+    # 1337 Console Text
+    print('I shall fuck Discord up')
+    await LoadCogs()
+
+# Load all the Cogs in Folder "Modules"
+async def LoadCogs():
+    for cog in [f.replace('.py', "") for f in os.listdir("Modules") if isfile(join("Modules", f))]:
+        try:
+            if not "__init__" in cog:
+                bot.load_extension("Modules." + cog)
+                print("Loaded {}".format(cog))
+        except Exception as e:
+            print("Couldn't load {}.".format(cog))
+            traceback.print_exc()
+
+if __name__ == '__main__':
+    bot.run(TOKEN, reconnect=True)
+else:
+    del bot
